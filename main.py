9b06e4589e71152a947c93334f74b5dc57451c5c
noel-sumini <ysm2820@gmail.com>
2025-12-10 22:06:50 +0900

    test added

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def create_todo(data:CreateTodoSchema):
 
 @app.get("/read", status_code=200)
 def read_todo(id:str):
+    """test"""
     cursor.execute(
         """
             SELECT * FROM todos 
